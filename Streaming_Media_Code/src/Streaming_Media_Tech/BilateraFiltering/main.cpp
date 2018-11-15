645f4b385bf2d1a2e1bd38aadadc984cb8cd80c3
sagacity-leichuan <sagacity_lc@163.com>
2018-11-15 23:56:32 +0800

    20181115-000

---
+++ Streaming_Media_Code/src/Streaming_Media_Tech/BilateraFiltering/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#pragma comment(lib,"opencv_world320.lib")
+
+using namespace std;
+using namespace cv;
+int main(int argc, char argv[])
+{
+	Mat src = imread("001.jpg");
+	if (!src.data)
+	{
+		cout << "open file failed!" << endl;
+		getchar();
+		return -1;
+	}
+	namedWindow("src");
+	moveWindow("src", 100, 100);
+	imshow("src", src);
+	Mat image;
+	int d = 3;
+	namedWindow("image");
+	moveWindow("image", 600, 100);
+	for (;;)
+	{
+
+		long long b = getTickCount();
+		bilateralFilter(src, image, d, d * 2, d / 2);
+		double sec = (double)(getTickCount() - b) / (double)getTickFrequency();
+		cout << "d = " << d << " sec is " << sec << endl;
+		imshow("image", image);
+		int k = waitKey(0);
+		if (k == 'd') d += 2;
+		else if (k == 'f')d -= 2;
+		else break;
+
+	}
+	waitKey(0);
+	return 0;
+}
